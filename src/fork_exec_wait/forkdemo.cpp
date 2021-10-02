d7b52cb6fc435db3250dee7cdf9f21813001554e
Burkov Egor <wooftown@yandex.ru>
2021-10-02 14:58:46 +0300

    Lab10 src

--- src/fork_exec_wait/forkdemo.cpp
+++ src/fork_exec_wait/forkdemo.cpp
@@ -4,12 +4,12 @@
 main()
 {
  int i;
-  if (fork()) {    /* I must be the parent  */
-      for(i=0; i<1000 ; i++)
-          printf("\t\tPARENT %d\n", i);
-      }
-       else {      /* I must be the child   */
-        for(i=0; i<1000 ; i++)
+if (fork()) {    /* I must be the parent  */
+    for(i=0; i<1000 ; i++)
+        printf("\t\tPARENT %d\n", i);
+    }
+else {      /* I must be the child   */
+    for(i=0; i<1000 ; i++)
             printf("CHILD %d\n",i);
-        }
-}      
+    }
+}
